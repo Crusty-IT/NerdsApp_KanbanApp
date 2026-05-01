c355b80774685c99d392bfae90aa5b53f6bf45c6
Tomasz Skorupski <skorupa7@o2.pl>
2026-05-01 03:49:11 +0200

    update api url for production backend

--- KanbanApp.Frontend/src/services/api.js
+++ KanbanApp.Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.PROD
-    ? 'https://shellty-kanban-api.onrender.com'
+    ? 'https://shellty-kanban.onrender.com'
     : 'http://localhost:5067';
 
 const api = axios.create({
